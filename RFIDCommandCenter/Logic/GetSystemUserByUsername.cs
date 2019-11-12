da3464686965907e5c948d60ebc604367000f794
eionsimonelli <eion.simonelli@rockets.utoledo.edu>
2019-11-11 19:17:53 -0500

    added systemuser methods

---
+++ RFIDCommandCenter/Logic/GetSystemUserByUsername.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RFIDCommandCenter.Logic
+{
+    public class GetSystemUserByUsername
+    {
+        public SystemUser Execute(string userName, DataContext context)
+        {
+            return context.SystemUsers.SingleOrDefault(x => x.Username == userName);
+        }
+    }
+}
--- RFIDCommandCenter/Program.cs
+++ RFIDCommandCenter/Program.cs
@@ -1,18 +1,19 @@
-﻿using System;
+﻿using RFIDCommandCenter.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace RFIDCommandCenter
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             using (var context = new DataContext())
             {
-                var location = context.Locations.Where(x => x.ID == 1).ToList();
-                var test = location;
+                var component = new Logic.GetSystemUserByUsername();
+                var test = component.Execute("cosimo", context);
             }
 
         }
