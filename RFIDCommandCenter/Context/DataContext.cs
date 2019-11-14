dfcc07f30ca4cc1ed627cc2bcb4c4f8dcff48de1
eionsimonelli <eion.simonelli@rockets.utoledo.edu>
2019-11-13 19:41:37 -0500

    adding additional methods

--- RFIDCommandCenter/Context/DataContext.cs
+++ RFIDCommandCenter/Context/DataContext.cs
@@ -4,6 +4,7 @@ namespace RFIDCommandCenter
     using System.Data.Entity;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Linq;
+    using RFIDCommandCenter.Models;
 
     public partial class DataContext : DbContext
     {
@@ -15,6 +16,7 @@ namespace RFIDCommandCenter
         public DbSet<Location> Locations { get; set; }
         public DbSet<SystemUser> SystemUsers { get; set; }
         public DbSet<Tag> Tags { get; set; }
+        public DbSet<TagLocationBridge> AllowedLocations { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
---
+++ RFIDCommandCenter/Logic/GetAllowedLocationIdsByTagIds.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RFIDCommandCenter.Logic
+{
+    public class GetAllowedLocationIdsByTagIds
+    {
+        public List<int> Execute(int tagId, DataContext dataContext)
+        {
+            return dataContext.AllowedLocations.Where(x => x.TagID == tagId).Select(x => x.LocationID).ToList();
+        }
+    }
+}
