a1eebf2085157185798cb8f5efd4a574d8c88323
eionsimonelli <eion.simonelli@rockets.utoledo.edu>
2019-11-27 09:52:24 -0500

    fixing save allowed locations logic

--- RFIDCommandCenter/Logic/SaveAllowedTagLocation.cs
+++ RFIDCommandCenter/Logic/SaveAllowedTagLocation.cs
@@ -8,14 +8,24 @@ namespace RFIDCommandCenter.Logic
 {
     public class SaveAllowedTagLocation
     {
-        public void Execute(int tagId, int locationId, DataContext context)
+        public void Execute(string tagName, List<string> locationName, DataContext context)
         {
-            var allowedLocation = context.AllowedLocations.SingleOrDefault(a => a.TagID == tagId && a.LocationID == locationId);
+            var locationIds = context.Locations.Where(l => locationName.Contains(l.LocationName)).Select(l => l.ID).ToList();
 
-            if (allowedLocation != null)
+            if (locationIds == null)
+                throw new UIClientException("There is no location with any of the specified location names");
+
+            var tag = context.Tags.SingleOrDefault(t => t.Name == tagName);
+
+            if (tag == null)
+                throw new UIClientException("There is no tag with the specified tag name");
+
+            var allowedLocations = locationIds.ConvertAll(x => new Models.TagLocationBridge { LocationID = x, TagID = tag.ID });
+
+            if (allowedLocations.Any(x => x.TagID == tag.ID && locationIds.Contains(x.LocationID))) 
                 throw new UIClientException("An allowed location with that ID and Location already exists");
 
-            context.AllowedLocations.Add(new Models.TagLocationBridge { TagID = tagId, LocationID = locationId });
+            context.AllowedLocations.AddRange(allowedLocations);
 
             context.SaveChanges();
         }
